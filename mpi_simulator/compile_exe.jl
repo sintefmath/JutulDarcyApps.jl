05efd4d1e63de6fc7bb5553fd6de1e615e963777
Olav Møyner <olav.moyner@gmail.com>
2024-02-15 13:50:30 +0100

    Enable bundling of lazy artifacts

--- mpi_simulator/compile_exe.jl
+++ mpi_simulator/compile_exe.jl
@@ -2,7 +2,8 @@ using PackageCompiler, Jutul, JutulDarcy
 # This can take a while.
 create_app("JutulDarcyMPI", "compiled_simulator",
     precompile_execution_file = "precompile_jutul_darcy_mpi.jl", # Precompilation script
-    force = true, # Delete existing files
-    incremental=true,
-    sysimage_build_args = Cmd(["-O2"]) # Set Julia flags for precompilation
+    force = true,                                                # Delete existing files
+    include_lazy_artifacts = true,                               # Bundle all artifacts
+    incremental=true,                                            # Add onto existing Julia sysimage
+    sysimage_build_args = Cmd(["-O2"])                           # Set Julia flags for precompilation
 )
