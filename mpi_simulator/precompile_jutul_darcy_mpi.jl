0f443a552ad25c86738d0dca784546c883c5870d
Olav Møyner <olav.moyner@gmail.com>
2024-02-15 13:46:40 +0100

    Make precompilation script more robust w.r.t. paths

--- mpi_simulator/precompile_jutul_darcy_mpi.jl
+++ mpi_simulator/precompile_jutul_darcy_mpi.jl
@@ -4,17 +4,21 @@ using JutulDarcy
 using LinearAlgebra
 using JutulDarcyMPI
 
-
-basepath = joinpath(pathof(JutulDarcy), "..", "..", "test")
+pth, = splitdir(pathof(JutulDarcy))
+basepath = joinpath(pth, "..", "test")
 for k in ["spe1", "egg", "spe9", "spe3"]
     empty!(ARGS)
     push!(ARGS, joinpath(basepath, "mrst", "$k.mat"))
-    JutulDarcyMPI.julia_main()
+    if isfile(ARGS[1])
+        JutulDarcyMPI.julia_main()
+    end
 end
 
 empty!(ARGS)
 push!(ARGS, joinpath("data", "SPE1", "BENCH_SPE1.DATA"))
-JutulDarcyMPI.julia_main()
+if isfile(ARGS[1])
+    JutulDarcyMPI.julia_main()
+end
 
 # You can add your own files here if you want certain physics / parsing to be
 # included in the final result.
